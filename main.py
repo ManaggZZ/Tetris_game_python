12be5fe5b1cbd7c445c59d89c8c01a0476123546
Manan <mananaggarwal27@gmail.com>
2025-12-09 20:44:37 +0530

    added the score function

--- main.py
+++ main.py
@@ -60,6 +60,7 @@ while True:     # This will run till the game is open
                 game.move_right()
             if event.key == pygame.K_DOWN and game.game_over == False:
                 game.move_down()
+                game.update_score(0, 1)
             if event.key == pygame.K_UP and game.game_over == False:
                 game.rotate()
         
@@ -77,6 +78,8 @@ while True:     # This will run till the game is open
             game.move_down()
     
     # Drawing
+    score_value_surface = title_font.render(str(game.score), True, Colors.white)    #* As the score is not statis
+
     screen.fill(Colors.dark_blue)
     screen.blit(score_surface, (365, 20, 50, 50))  #! blit() method is block image transfer
     screen.blit(next_suface, (375, 180, 50, 50))
@@ -85,6 +88,7 @@ while True:     # This will run till the game is open
         screen.blit(game_over_surface, (320, 450, 50, 50))
 
     pygame.draw.rect(screen, Colors.light_blue, score_rect, 0, 10)
+    screen.blit(score_value_surface, score_value_surface.get_rect(centerx = score_rect.centerx, centery = score_rect.centery))
     pygame.draw.rect(screen, Colors.light_blue, next_rect, 0, 10)
     game.draw(screen)
 
